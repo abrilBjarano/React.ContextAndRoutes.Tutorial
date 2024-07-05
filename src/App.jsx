3bf29f32b9f675b903cf53e56b19e0ff64838bb5
abrilxcx <abrlbjrn@gmail.com>
2024-07-05 12:44:28 -0600

    Created Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { AboutPage } from "./AboutPage";
+import { LoginPage } from "./LoginPage";
+
+
 export const App = () => {
   return (
     <>
       <h1>App</h1>
       <hr />
+
+      <Routes>
+        <Route path="/" element={ <HomePage/> } />
+        <Route path="about" element={ <AboutPage/> } />
+        <Route path="login" element={ <LoginPage/> } />
+
+        // el comodín en inglés se conoce como wildcard
+        <Route path="/*" element={ <Navigate to="/" /> } />
+      </Routes>
     </>
   )
 }
