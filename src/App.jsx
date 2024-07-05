a9ac3bd48e49a3960051cf540870931b650f8d59
abrilxcx <abrlbjrn@gmail.com>
2024-07-05 13:02:06 -0600

    Created Navbar.jsx and implemented to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Link, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage";
 import { LoginPage } from "./LoginPage";
+
+import { Navbar } from "./Navbar";
 
 
 export const App = () => {
   return (
     <>
       <h1>App</h1>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
+      <Navbar />
       <hr />
 
       {/* 🧠 Pensandolo bien, estas rutas se verían más organizadas en un componente separado */}
