b208e236b926deb562a60df09e2435f673695b8d
abrilxcx <abrlbjrn@gmail.com>
2024-07-05 12:56:44 -0600

    Created index.css and implemented into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import './index.css';
+
+import { App } from './App';
 
 
 const router = createBrowserRouter([
