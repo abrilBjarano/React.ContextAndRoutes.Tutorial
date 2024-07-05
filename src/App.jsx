d63490dfe6027ee35ecb48da3dd54d97c8d2a602
abrilxcx <abrlbjrn@gmail.com>
2024-07-05 12:54:48 -0600

    Added Links in App.jsx
    and fixed some commdnets

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage";
 import { LoginPage } from "./LoginPage";
@@ -8,14 +8,18 @@ export const App = () => {
   return (
     <>
       <h1>App</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/login">Login</Link>
       <hr />
 
+      {/* 🧠 Pensandolo bien, estas rutas se verían más organizadas en un componente separado */}
       <Routes>
         <Route path="/" element={ <HomePage/> } />
         <Route path="about" element={ <AboutPage/> } />
         <Route path="login" element={ <LoginPage/> } />
 
-        // el comodín en inglés se conoce como wildcard
+        {/* el comodín en inglés se conoce como wildcard */}
         <Route path="/*" element={ <Navigate to="/" /> } />
       </Routes>
     </>
