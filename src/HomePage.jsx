083576512b5233d5a1311f373cd873fc6d5a2ebb
abrilxcx <abrlbjrn@gmail.com>
2024-07-09 13:18:48 -0600

    Added useContext to Login and HomePage and also added a button in order to change the user

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,19 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext";
+
 export const HomePage = () => {
-   return (
-     <>
-       <h1>HomePage</h1>
-       <hr />
-     </>
-   )
+
+    const { user } = useContext( UserContext );
+
+    return (
+      <>
+        <h1>HomePage { user?.name }</h1>
+        <hr />
+
+        <pre> 
+          { JSON.stringify( user, null, 4 ) }
+        </pre>
+      </>
+    )
  }
  
--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,9 +1,31 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
-   return (
-     <>
-       <h1>LoginPage</h1>
-       <hr />
-     </>
-   )
+
+    const { user, setUser }  = useContext( UserContext );
+
+    const onSetUser = () => {
+      setUser({
+        id: 123,
+        name: 'Liss',
+        mail: 'liss@gmail.com'
+      });
+    };
+
+    return (
+      <>
+        <h1>LoginPage</h1>
+        <hr />
+
+        <pre>{ JSON.stringify( user, null, 4 ) }</pre>
+
+        <button 
+          className="btn btn-primary"
+          onClick={ onSetUser }>
+          Set user
+        </button>
+      </>
+    )
  }
  
