4743fc26dd54062a7d5982427fe1c9022d7b19ee
abrilxcx <abrlbjrn@gmail.com>
2024-07-09 13:18:17 -0600

    Upgraded information from UserProvider
    Create a useState instead a const with the context information. Also send the setUser to value in order to change user between the components.

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import { UserContext } from './UserContext';
 
-const user = {
-   id: 123,
-   name: 'Abril',
-   mail: 'abril@gmail.com'
-}
+// const user = {
+//    id: 123,
+//    name: 'Abril',
+//    mail: 'abril@gmail.com'
+// }
 
 export const UserProvider = ({ children }) => {
 
+   const [ user, setUser ] = useState();
+
+
    return (
       // Ese value, es la infomarción que es la que va a estar en el lugar centralizado para compartir con los demás componentes que requieran esa info. En este caso, la información es el user.
-      <UserContext.Provider value={{ user }}>
+      <UserContext.Provider value={{ user, setUser }}>
          { children }
       </UserContext.Provider>
    )
